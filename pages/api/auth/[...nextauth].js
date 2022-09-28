8540c3f5771769e8e952148d0327430ccc840963
iri-na <imcarcam@uwaterloo.ca>
2022-09-28 05:16:21 -0400

    Send Verification Emails and Welcome Emails For Magic Link Auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,26 +1,75 @@
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import nodemailer from 'nodemailer';
+import Handlebars from 'handlebars';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// For sending magic sign-in links
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+    secure: true,
+});
+
+const emailsDir = path.resolve(process.cwd(), 'emails');
+
+// Verification for Magic Link
+const sendVerificationRequest = ({ identifier, url }) => {
+    const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
+        encoding: 'utf8',
+    });
+    const emailTemplate = Handlebars.compile(emailFile);
+    transporter.sendMail({
+        from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
+        to: identifier,
+        subject: 'Your sign-in link for SupaVacation',
+        html: emailTemplate({
+            base_url: process.env.NEXTAUTH_URL,
+            signin_url: url,
+            email: identifier,
+        }),
+    });
+};
+
+// Welcome Email After Logging In
+const sendWelcomeEmail = async ({ user }) => {
+    const { email } = user;
+
+    try {
+        const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
+            encoding: 'utf8',
+        });
+        const emailTemplate = Handlebars.compile(emailFile);
+        await transporter.sendMail({
+            from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
+            to: email,
+            subject: 'Welcome to SupaVacation! 🎉',
+            html: emailTemplate({
+                base_url: process.env.NEXTAUTH_URL,
+                support_email: 'support@themodern.dev',
+            }),
+        });
+    } catch (error) {
+        console.log(`❌ Unable to send welcome email to user (${email})`);
+    }
+};
+
 export default NextAuth({
     providers: [
         // Magic Link Authentication
         EmailProvider({
-            server: {
-                host: process.env.EMAIL_SERVER_HOST,
-                port: process.env.EMAIL_SERVER_PORT,
-                auth: {
-                    user: process.env.EMAIL_SERVER_USER,
-                    pass: process.env.EMAIL_SERVER_PASSWORD,
-                },
-            },
-            from: process.env.EMAIL_FROM,
-            maxAge: 10 * 60, // Magic links are valid for 10 min only
+            maxAge: 10 * 60,
+            sendVerificationRequest,
         }),
     ],
     adapter: PrismaAdapter(prisma),
@@ -31,4 +80,5 @@ export default NextAuth({
         error: '/',
         verifyRequest: '/',
     },
+    events: { createUser: sendWelcomeEmail },
 });
