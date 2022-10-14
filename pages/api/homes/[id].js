ab351828c99828022cc8427729d8d525cda4dc46
iri-na <imcarcam@uwaterloo.ca>
2022-10-14 05:17:39 -0400

    Create DELETE Endpoint

--- pages/api/homes/[id].js
+++ pages/api/homes/[id].js
@@ -1,5 +1,5 @@
 import { getSession } from 'next-auth/react';
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -34,11 +34,24 @@ export default async function handler(req, res) {
             res.status(500).json({ message: 'Something went wrong' });
         }
     }
+
+    // Delete home
+    else if (req.method === 'DELETE') {
+        try {
+            const home = await prisma.home.delete({
+                where: { id },
+            });
+            res.status(200).json(home);
+        } catch (e) {
+            res.status(500).json({ message: 'Something went wrong' });
+        }
+    }
+
     // HTTP method not supported!
     else {
-        res.setHeader('Allow', ['PATCH']);
+        res.setHeader('Allow', ['PATCH', 'DELETE']);
         res
             .status(405)
-            .json({ message: `HTTP method ${req.method} is not supported.` });
+            .json({ message: `HTTP method ${ req.method } is not supported.` });
     }
 }
