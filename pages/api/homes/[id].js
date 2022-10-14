e6392c86e29406d64ca80a412a3a432576166d15
iri-na <imcarcam@uwaterloo.ca>
2022-10-14 05:21:36 -0400

    Remove Image From Supabase Storage on DELETE

--- pages/api/homes/[id].js
+++ pages/api/homes/[id].js
@@ -1,7 +1,13 @@
 import { getSession } from 'next-auth/react';
 import { PrismaClient } from '@prisma/client';
+import { createClient } from '@supabase/supabase-js';
 
 const prisma = new PrismaClient();
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
 
 export default async function handler(req, res) {
     // Check if user is authenticated
@@ -41,6 +47,11 @@ export default async function handler(req, res) {
             const home = await prisma.home.delete({
                 where: { id },
             });
+            // Remove image from Supabase storage
+            if (home.image) {
+                const path = home.image.split(`${process.env.SUPABASE_BUCKET}/`)?.[1];
+                await supabase.storage.from(process.env.SUPABASE_BUCKET).remove([path]);
+            }
             res.status(200).json(home);
         } catch (e) {
             res.status(500).json({ message: 'Something went wrong' });
